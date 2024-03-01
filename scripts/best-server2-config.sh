15404076efc6e615253deb47aeb2ac704028ac28
NielsVerhoevenPXL <11601010@studen.pxl.be>
2024-03-01 22:22:51 +0100

    kdo

---
+++ scripts/best-server2-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# maak een docker netwerk aan
+docker network create cart-network
+
+
+# kijk of er al containers running zijn en stop ze
+if [ $(docker ps -q | wc -l) -gt 0 ]; then
+    docker stop $(docker ps -q)
+fi
+
+# run alle containers
+docker run -d --rm --env-file ~/online-boutique/src/variables.env --env PORT=7070 --network cart-network --name cartservice -p 7070:7070 public.ecr.aws/j1n2c2p2/microservices-demo/cartservice:latest
+docker run -d --rm --env-file ~/online-boutique/src/variables.env --env PORT=50051 --network cart-network --name shippingservice -p 50051:50051 public.ecr.aws/j1n2c2p2/microservices-demo/shippingservice:latest
+docker run -d --rm --env-file ~/online-boutique/src/variables.env --env PORT=7000  --network cart-network --name currencyservice -p 50000:50000 -p 60000:60000 -p 7000:7000 public.ecr.aws/j1n2c2p2/microservices-demo/currencyservice:latest
--- scripts/general-config.sh
+++ scripts/general-config.sh
@@ -2,6 +2,7 @@
 yum install git -y
 git clone https://github.com/NielsVerhoevenPXL/online-boutique.git
 
+#install docker
 yum install docker -y
 service docker start
 usermod -a -G docker ec2-user
