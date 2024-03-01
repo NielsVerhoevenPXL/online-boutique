94d82c9555ee24f1b6d8c4265765fd2332bc9e82
NielsVerhoevenPXL <11601010@studen.pxl.be>
2024-03-01 21:36:37 +0100

    "editing config file "

--- scripts/best-server1-config.sh
+++ scripts/best-server1-config.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-# defineer variabelen
-server2ip="172.31.60.72"
-server3ip="172.31.53.10"
-server4ip="172.31.55.191"
+# lees ips uit bestand
+i=1
+while IFS= read -r line; do
+    var="server${i}ip"
+    eval "$var=$(echo "$line" | awk '{print $1}')"
+    i=$((i+1))
+done < "ips"
 
+# voeg hosts toe aan /etc/hosts
 echo "$server2ip  cartservice" | sudo tee -a /etc/hosts
 echo "$server2ip  shippingservice" | sudo tee -a /etc/hosts
 echo "$server2ip  currencyservice" | sudo tee -a /etc/hosts
 echo "$server3ip  checkoutservice" | sudo tee -a /etc/hosts
 echo "$server4ip  adservice" | sudo tee -a /etc/hosts
 
+# maak een docker netwerk aan
 docker network create front-network
 
+
+# kijk of er al containers running zijn en stop ze
+if [ $(docker ps -q | wc -l) -gt 0 ]; then
+    docker stop $(docker ps -q)
+fi
+
+# run alle containers
 docker run -d --rm --env-file ~/online-boutique/src/variables.env --env PORT=80 --network front-network --name frontend -p 8080:80 public.ecr.aws/j1n2c2p2/microservices-demo/frontend:latest
 
 docker run -d --rm --env-file ~/online-boutique/src/variables.env --env PORT=8081 --network front-network --name recommendationservice -p 8081:8081 public.ecr.aws/j1n2c2p2/microservices-demo/recommendationservice:latest
