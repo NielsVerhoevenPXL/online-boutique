1cb0abc0d494b3ea004cbf57096c7ab1b768c07b
NielsVerhoevenPXL <11601010@studen.pxl.be>
2024-03-02 09:23:00 +0100

    edit

--- scripts/best-server4-config.sh
+++ scripts/best-server4-config.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo yum install git -y
+git clone https://github.com/NielsVerhoevenPXL/online-boutique.git
+
+
+docker build -t loadgenerator online-boutique/src/loadgenerator/.
+
 # maak een docker netwerk aan
 docker network create ad-network
 
@@ -16,9 +22,11 @@ while IFS= read -r line; do
     i=$((i+1))
 done < "ips"
 
+
+
 # run alle containers
 docker run -d --rm --env FRONTEND_ADDR="$server1ip:8080" --env USERS=10 --network ad-network --name loadgenerator loadgenerator
-docker run -d --rm --env-file variables.env --env PORT=9555 --network ad-network --name adservice -p 9555:9555 adservice
+docker run -d --rm --env-file variables.env --env PORT=9555 --network ad-network --name adservice -p 9555:9555 public.ecr.aws/j1n2c2p2/microservices-demo/adservice:latest
 
 
 docker exec loadgenerator sh -c "echo '$server1ip  frontend' | tee -a /etc/hosts"
